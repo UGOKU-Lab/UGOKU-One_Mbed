a04782b1e4a6c706cf498b7c6717efc463b5f354
Shohei Sano <shohei.s.prerobo17@gmail.com>
2024-02-17 13:56:20 +0900

    add config headers.

---
+++ channel.h
@@ -0,0 +1,20 @@
+#ifndef UGOKUONE_CHANNEL_H
+#define UGOKUONE_CHANNEL_H
+
+#define CH_SYN 0
+#define CH_ACK 1
+
+#define CH_LED1 10
+#define CH_LED2 11
+
+#define CH_MOTOR1 20
+#define CH_MOTOR2 21
+
+#define CH_SERVO1 30
+#define CH_SERVO2 31
+
+#define CH_SENSOR 40
+
+#define CH_MAX 40
+
+#endif
---
+++ pin.h
@@ -0,0 +1,21 @@
+#ifndef UGOKUONE_PIN_H
+#define UGOKUONE_PIN_H
+
+// LED out pin
+#define PIN_LED0 PA_0
+#define PIN_LED1 PA_6
+#define PIN_LED2 PA_7
+
+// Motor PWM out pin
+#define PIN_MOTOR1 PB_1
+#define PIN_MOTOR2 PB_0
+#define PIN_SERVO1 PA_3
+#define PIN_SERVO2 PA_4
+
+// Switch in pin
+#define PIN_PUSH PF_1
+#define PIN_TOGGLE PB_3
+
+#define PIN_ANALOG1 PA_0
+
+#endif
